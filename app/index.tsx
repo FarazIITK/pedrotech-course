d437541f90d60fba2cb5ca95fb0b85d575c74c0d
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-05 13:51:31 +0530

    Used StyleSheet for styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,30 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link
-        href={"/login"}
-        style={{
-          width: 100,
-          height: 40,
-          borderRadius: 5,
-          backgroundColor: "grey",
-          textAlign: "center",
-          lineHeight: 40,
-          marginTop: 10,
-        }}
-      >
+      <Link href={"/login"} style={styles.navButton}>
         Login screen
       </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navButton: {
+    width: 100,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: "grey",
+    textAlign: "center",
+    lineHeight: 40,
+    marginTop: 10,
+  },
+});
