7c98630218a2149d809a79d5c6a6d244cab5c5da
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-05 13:42:15 +0530

    Implemented simple navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,20 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link
+        href={"/login"}
+        style={{
+          width: 100,
+          height: 40,
+          borderRadius: 5,
+          backgroundColor: "grey",
+          textAlign: "center",
+          lineHeight: 40,
+          marginTop: 10,
+        }}
+      >
+        Login screen
+      </Link>
     </View>
   );
 }
