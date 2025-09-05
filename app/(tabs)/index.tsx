3fa257fa51b8a1fb691d9cc2a3c1972b323f1512
Faraz Husain <fhusain@wr.iitr.ac.in>
2025-09-05 16:21:39 +0530

    Added tabs component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/login"} style={styles.navButton}>
-        Login screen
-      </Link>
     </View>
   );
 }
@@ -17,14 +13,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  navButton: {
-    width: 100,
-    height: 40,
-    borderRadius: 5,
-    backgroundColor: "grey",
-    textAlign: "center",
-    lineHeight: 40,
-    marginTop: 10,
   },
 });
